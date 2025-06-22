e8eb4d1e5066d858e403d5919c2bf204261cfe04
Benfranklin m s <bfranklinms@gmail.com>
2025-06-22 23:03:06 +0530

    checked

---
+++ server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def Home():
+    return jsonify({"message": "server running"});
+
+
+if(__name__ == "__main__"):
+    app.run(debug=True)
